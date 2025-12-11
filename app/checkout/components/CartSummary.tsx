5b2abf40c8a09cf2c8649b3af2f29ef031a81345
bhavikasoni3796 <bhavikasoni3796@gmail.com>
2025-12-11 14:13:08 +0530

    installed lucide-react lib| created home page

---
+++ app/checkout/components/CartSummary.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartSummary = () => {
+  return <div></div>;
+};
+
+export default CartSummary;
---
+++ app/checkout/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckoutApp = () => {
+  return <div></div>;
+};
+
+export default CheckoutApp;
