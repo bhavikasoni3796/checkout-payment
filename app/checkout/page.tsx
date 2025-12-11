33c763c4b147545695c1a1fb2db4c9458aae150a
bhavikasoni3796 <bhavikasoni3796@gmail.com>
2025-12-11 19:27:51 +0530

    created basic checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import PaymentForm from "./components/PaymentForm";
+import CartSummary from "./components/CartSummary";
 
 const CheckoutApp = () => {
-  return <div></div>;
+  return (
+    <div className="py-12 px-4 min-h-screen">
+      <div className="max-w-6xl mx-auto">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-slate-800  mb-2">Checkout</h1>
+          <p className="text-slate-600">Complete your purchase securely</p>
+        </div>
+        <div className="grid lg:grid-cols-2 gap-8">
+          <div>
+            <PaymentForm />
+          </div>
+          <div>
+            <CartSummary />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutApp;
